9106c0f4541a6638a8f5fa8a946e2d9fff8624cf
mauro-balades <mauro.balades@tutanota.com>
2021-12-22 19:34:03 +0100

    finaly solved the installation script

--- install/fetch.sh
+++ install/fetch.sh
@@ -7,13 +7,14 @@ cd bash-plusplus
 if [[ ! "$1" == "" ]];
 then
   sudo make install -B PREFIX="../$1/"
-  cd "../$1/"
+  cd "../$1/bash++"
+  pwd
   sudo mv libs/* .
   rm -rf libs
 
-  cd ..
-  chmod +x "../$1"
-  cd "../$1"
+  pwd
+  sudo chown $USER:$USER "../$1/bash++"
+  sudo chmod  -R 777 "../$1/bash++/"
 else
   sudo make -B install
 fi
